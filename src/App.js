21048ed426720d318bb37c4402c2bf5f83954c6a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-04 11:43:26 +0530

    Added </DragDropContext> wrapper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import {finalSpaceCharacters} from './util/data'
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 function App() {
   return (
     <div className="App">
     <header className="App-header">
       <h1>Final Space Characters</h1>
-      <ul className="characters">
-        {finalSpaceCharacters.map(({id, name, thumb}) => {
-          return (
-            <li key={id}>
-              <div className="characters-thumb">
-                <img src={thumb} alt={`${name} Thumb`} />
-              </div>
-              <p>
-                { name }
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+
+      <DragDropContext>
+        <ul className="characters">
+          {finalSpaceCharacters.map(({id, name, thumb}) => {
+            return (
+              <li key={id}>
+                <div className="characters-thumb">
+                  <img src={thumb} alt={`${name} Thumb`} />
+                </div>
+                <p>
+                  { name }
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </DragDropContext>
     </header>
     <p>
       Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
