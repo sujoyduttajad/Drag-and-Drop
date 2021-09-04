4500868307ef709a0d3217cbd9f4446ccef5af07
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-04 11:25:25 +0530

    Added the data array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import {finalSpaceCharacters} from './util/data'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <header className="App-header">
+      <h1>Final Space Characters</h1>
+      <ul className="characters">
+        {finalSpaceCharacters.map(({id, name, thumb}) => {
+          return (
+            <li key={id}>
+              <div className="characters-thumb">
+                <img src={thumb} alt={`${name} Thumb`} />
+              </div>
+              <p>
+                { name }
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+    <p>
+      Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
+    </p>
+  </div>
   );
 }
 
